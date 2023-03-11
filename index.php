818df7379297a21199a614f35e60155f3fcee1cd
ttiennot <113522386+ttiennot@users.noreply.github.com>
2023-03-11 18:19:37 +0100

    exo 6

--- index.php
+++ index.php
@@ -1,21 +1,15 @@
 <!DOCTYPE html>
-<html lang="fr">
-
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>exo 10</title>
-    <link rel="stylesheet" href="main.css">
+    <title>exo 6</title>
 </head>
-
 <body>
-    <button id="button">fonction tab 1</button>
-    <button id="button2">fonction tab 2</button>
+    <button id="button">cliquez ici ! (soustrait)</button>
+    <button id="button2">cliquez ici ! (affiche)</button>
 </body>
 <script src="main.js"></script>
-<script type="text/javascript">
-    
-</script>
-
+<script type="text/javascript"></script> 
 </html>
--- main.js
+++ main.js
@@ -1,23 +1,20 @@
 maDiv = document.getElementById("button");
-maDiv.addEventListener("click", boucle);
-maDiv = document.getElementById("button2");
-maDiv.addEventListener("click", boucle2);
-// tab.length
-function boucle(){
-    let tab=new Array(3);
-    for(var i = 0; i < 3; i++){
-        tab[i]= i*i;
-    }
-    alert("voici les éléments du tableau :"+ tab[0] + ',' + tab[1] + ',' + tab[2]);
+maDiv.addEventListener("click", soustrait);
 
-    
+maDiv = document.getElementById("button");
+maDiv.addEventListener("click", affiche);
+
+var tab=[-2, 1, 4];
+function soustrait(x){
+    if(x >= 0){
+        return x - 2;
+    } else {
+        return "Nombre négatif !";
+    }
 }
 
-function boucle2(){
-    var nbr = window.prompt("saisissez la longueur souhaitée pour le tableau :");
-    let tab=new Array(nbr);
-    for(var i = 0; i < nbr; i++){
-        tab[i]= i*i;
+function affiche(){
+    for(var i = 0; i < tab.length; i++){
+        alert("voici la valeur du tableau à l'emplacement" + i + ":" + tab[i]);
     }
-    alert("voici les éléments du tableau :"+ tab);  
 }
